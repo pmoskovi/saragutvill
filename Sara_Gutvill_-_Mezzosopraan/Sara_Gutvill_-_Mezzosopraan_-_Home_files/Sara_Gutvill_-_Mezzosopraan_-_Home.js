0781bdea58e287d0d9a43474d6d9c3a623f8440e
Nicola <nicola@dyne.org>
2016-12-03 21:05:06 +0100

    WIP ...under pressure

--- Sara_Gutvill_-_Mezzosopraan/Sara_Gutvill_-_Mezzosopraan_-_Home_files/Sara_Gutvill_-_Mezzosopraan_-_Home.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20150810
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Sara_Gutvill_-_Mezzosopraan_-_Home_files/Sara_Gutvill_-_Mezzosopraan_-_HomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
